7a4c0cf1c5982bb368ecde93c140612ddccf6cb5
Cyrhyl <navalcyrhylkurt@gmail.com>
2026-03-24 15:31:20 +0800

    initialized schedule controllers

---
+++ src/model/schedule/RequestSchedule.java
@@ -0,0 +1,22 @@
+package model.schedule;
+
+public class RequestSchedule extends Schedule {
+    private String dateRequested;
+    private String studentNumber;
+
+    public void load(int ID, String roomCode, String sectionKey, String courseCode, String facultyID, String timeIn,
+            String timeOut, String scheduledDay, String status, int isArchived, String dateRequested, String studentNumber) {
+        // TODO Auto-generated method stub
+        super.load(ID, roomCode, sectionKey, courseCode, facultyID, timeIn, timeOut, scheduledDay, status, isArchived);
+        this.dateRequested = dateRequested;
+        this.studentNumber = studentNumber;
+    }
+
+    public String getDateRequested(){
+        return dateRequested;
+    }
+
+    public String getStudentRequested(){
+        return studentNumber;
+    }
+}
