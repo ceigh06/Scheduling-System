0210f93cb8c641079343366bfd1b64a44a86da9a
Cyrhyl <navalcyrhylkurt@gmail.com>
2026-03-29 20:12:04 +0800

    undo merge resolve

--- src/service/ScheduleValidator.java
+++ src/service/ScheduleValidator.java
@@ -1,18 +1,20 @@
 package service;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import model.schedule.Schedule;
 
 public class ScheduleValidator {
 
     public static boolean isOverlapping(String timeIn, String timeOut, List<Schedule> schedules) {
-        LocalTime timeStart = LocalTime.parse(timeIn);
-        LocalTime timeStop = LocalTime.parse(timeOut);
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("h:mm a");
+        LocalTime timeStart = LocalTime.parse(timeIn, format);
+        LocalTime timeStop = LocalTime.parse(timeOut, format);
 
         for (Schedule sched : schedules) {
-            LocalTime schedTimeIn = LocalTime.parse(sched.getTimeIn());
-            LocalTime schedTimeOut = LocalTime.parse(sched.getTimeOut());
+            LocalTime schedTimeIn = LocalTime.parse(sched.getTimeIn(), format);
+            LocalTime schedTimeOut = LocalTime.parse(sched.getTimeOut(), format);
 
             boolean overlaps = timeStart.isBefore(schedTimeOut) && timeStop.isAfter(schedTimeIn);
             
