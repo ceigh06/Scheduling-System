a4bb8596cfd33e43b66837aed00efc8fe7804706
Alexander Macale <alexander.macale1@gmail.com>
2026-03-31 17:17:39 +0800

    Moved Reports to new Admin Folder and Fixed admin connection to Log In Validator and Controller

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-import java.sql.SQLException;
-
-import controller.faculty.FacultyController;
 import controller.login.LoginController;
+import java.sql.SQLException;
 import utilities.DBConnection;
 import view.common.MainFrame;
-import view.common.Report1;
-import view.landing.Landing;
-import view.landing.Login;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        DBConnection connection = new DBConnection("LAPTOP-81CGQV8U\\SQLEXPRESS", "SchedulingSystem", "sa", "a");
+        DBConnection connection = new DBConnection("ALEX\\SQLEXPRESS", "SchedulingSystem", "sa", "1234");
 
         MainFrame.init();
         new LoginController();
