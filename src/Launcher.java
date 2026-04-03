1a4ccc4204b79440559ed7468a8cd957e3d78d29
Alexander Macale <alexander.macale1@gmail.com>
2026-04-03 15:07:07 +0800

    ignore

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,14 +5,8 @@ import view.common.MainFrame;
 
 public class Launcher {
     public static void main(String[] args) throws SQLException {
-<<<<<<< HEAD
         DBConnection connection = new DBConnection("ALEX\\SQLEXPRESS",
                 "SchedulingSystem", "sa", "1234");
-=======
-        DBConnection connection = new DBConnection("LAPTOP-81CGQV8U\\SQLEXPRESS",
-                "SchedulingSystem", "sa", "a");
-
->>>>>>> ea1b68091c6ad32e4e6f1440d3dd4236c70fb122
         MainFrame.init();
         new LoginController();
 
