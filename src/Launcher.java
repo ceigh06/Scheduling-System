6aa7b1b495861bca36b2e7c1a3e51c5b22773b0a
Cyrhyl <navalcyrhylkurt@gmail.com>
2026-04-05 02:24:56 +0800

    successfully integrated search rooms for faculty

--- src/Launcher.java
+++ src/Launcher.java
@@ -5,8 +5,8 @@ import view.common.MainFrame;
 
 public class Launcher {
     public static void main(String[] args) throws SQLException {
-        DBConnection connection = new DBConnection("Zia\\SQLEXPRESS",
-                "SchedulingSystemUPDATED", "sa", "12345");
+        DBConnection connection = new DBConnection("LAPTOP-81CGQV8U\\SQLEXPRESS",
+                "SchedulingSystem", "sa", "a");
         MainFrame.init();
         new LoginController();
 
